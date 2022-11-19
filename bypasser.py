f8031a99019531a6ac1d66cd99d5af16ede9a45e
CODE-BUDDY <111114081+BUDDY1609@users.noreply.github.com>
2022-11-19 15:25:35 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -20,12 +20,12 @@ import os
 ##########################################################
 # ENVs
 
-GDTot_Crypt = os.environ.get("CRYPT","b0lDek5LSCt6ZjVRR2EwZnY4T1EvVndqeDRtbCtTWmMwcGNuKy8wYWpDaz0%3D")
+GDTot_Crypt = os.environ.get("CRYPT","")
 Laravel_Session = os.environ.get("Laravel_Session","")
 XSRF_TOKEN = os.environ.get("XSRF_TOKEN","")
 DCRYPT = os.environ.get("DRIVEFIRE_CRYPT","")
-KCRYPT = os.environ.get("KOLOP_CRYPT","aWFicnVaNWh4TThRbzFqdkE2U2FKNmJOTWhvWkZmbWswaUFadTB5NXJ3RT0%3D")
-HCRYPT = os.environ.get("HUBDRIVE_CRYPT","Q29hdlpLUEZTSEJLUjVZRkZQSExLODFuWGVudUlNK0ZPZlZmS1hENWxZVT0%3D")
+KCRYPT = os.environ.get("KOLOP_CRYPT","")
+HCRYPT = os.environ.get("HUBDRIVE_CRYPT","")
 KATCRYPT = os.environ.get("KATDRIVE_CRYPT","")
 
 
