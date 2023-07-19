df08b58e80c38535e049e3beca2f4d83b6e478d0
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-07-19 07:38:43 +0530

    Update bypasser.py (#89)
    Xpshort Ref domain Changed

--- bypasser.py
+++ bypasser.py
@@ -1574,7 +1574,7 @@ def xpshort(url):
     url = url[:-1] if url[-1] == '/' else url
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
-    ref = "https://www.newviralsong.com/"
+    ref = "https://blog.entirelybiz.com/"
     h = {"referer": ref}
     resp = client.get(final_url,headers=h)
     soup = BeautifulSoup(resp.content, "html.parser")
