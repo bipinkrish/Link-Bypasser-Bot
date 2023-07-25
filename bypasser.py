9cec4af35d8619dc67597e8e5684f7686621f7a1
Sujal Patel <76617735+SujalPatel75@users.noreply.github.com>
2023-07-25 15:47:32 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -1574,7 +1574,7 @@ def xpshort(url):
     url = url[:-1] if url[-1] == '/' else url
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
-    ref = "https://blog.entirelybiz.com/"
+    ref = "https://www.paisaking.in/"
     h = {"referer": ref}
     resp = client.get(final_url,headers=h)
     soup = BeautifulSoup(resp.content, "html.parser")
