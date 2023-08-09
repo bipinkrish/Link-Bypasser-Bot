a08a8804339bad4a885ffba5b6f5a18fe37b7da6
TheKVT <78886764+TheKVT@users.noreply.github.com>
2023-08-09 17:00:53 +0530

    Update bypasser.py (#96)

--- bypasser.py
+++ bypasser.py
@@ -998,7 +998,7 @@ def shortingly(url):
 # Gyanilinks - gtlinks.me
 
 def gyanilinks(url):
-    DOMAIN = "https://go.theforyou.in/"
+    DOMAIN = "https://go.hipsonyc.com/"
     client = cloudscraper.create_scraper(allow_brotli=False)
     url = url[:-1] if url[-1] == '/' else url
     code = url.split("/")[-1]
