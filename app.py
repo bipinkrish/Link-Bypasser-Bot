02dd60bc4a28213005a1a97925bf821295abf22d
SujalPatel <sujalpatel7574@gmail.com>
2023-08-10 16:02:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,6 +70,6 @@ def index():
     return render_template("index.html", result=None, shortened_links=shortened_links.split(",") if shortened_links else None)
 
 
-    if __name__ == "__main__":
-       port = int(os.environ.get("PORT", 5000))
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
