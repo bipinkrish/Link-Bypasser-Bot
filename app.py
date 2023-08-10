49b713fd019c1ac7f48bff5ea47c3967346fe3a9
Sujal Patel <76617735+SujalPatel75@users.noreply.github.com>
2023-08-10 13:57:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,10 +15,18 @@ from texts import HELP_TEXT
 from ddl import ddllist, direct_link_generator
 from time import time
 
-
 app = Flask(__name__)
 
-# Function to handle link bypassing
+def handle_index(ele):
+    result = bypasser.scrapeIndex(ele)
+
+# Rest of your existing functions here
+
+def store_shortened_links(link):
+    with open('shortened_links.txt', 'a') as file:
+        file.write(link + '\n')
+
+
 def loop_thread(url):
     urls = []
     urls.append(url)
@@ -33,7 +41,7 @@ def loop_thread(url):
             return
         elif bypasser.ispresent(ddllist, ele):
             try:
-                temp = ddl.direct_link_generator(ele)
+                temp = direct_link_generator(ele)
             except Exception as e:
                 temp = "**Error**: " + str(e)
         else:
@@ -46,22 +54,20 @@ def loop_thread(url):
             link = link + temp + "\n\n"
 
     return link
-    # ... (rest of the function remains unchanged)
 
-# Function to store the shortened links in cookies
-def store_shortened_links(link):
-    result = bypasser.scrapeIndex(ele)
-    # ... (rest of the function remains unchanged)
-
-# Main route to display the form and handle the link bypassing
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         url = request.form.get("url")
         result = loop_thread(url)
-        shortened_links = request.cookies.get('shortened_links')
-        return render_template("index.html", result=result, shortened_links=shortened_links.split(","))
-    return render_template("index.html", result=None, shortened_links=None)
+
+        resp = make_response(render_template("index.html", result=result))
+        resp.set_cookie('shortened_links', result)
+
+        return resp
+
+    shortened_links = request.cookies.get('shortened_links')
+    return render_template("index.html", result=None, shortened_links=shortened_links.split(",") if shortened_links else None)
 
 if __name__ == "__main__":
     print("Web Application Starting")
