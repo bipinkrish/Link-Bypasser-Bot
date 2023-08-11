547f589c9f28ed1611ecabd3bc3b94e0b92935dd
sujal <sujalpatel7574@gmail.com>
2023-08-11 17:34:41 +0530

    Added feature to preview old bypassed link
    Now, you can view your old bypassed link in a list on that website, this uses cookies to store that shortned link on your device

--- app.py
+++ app.py
@@ -1,30 +1,28 @@
 from flask import Flask, request, render_template, make_response
 import bypasser
-import threading
 import re
 import os
-from texts import gdrivetext, otherstext, ddltext, shortnertext, HELP_TEXT  
 import pyrogram
-from pyrogram import Client,filters
-from pyrogram.types import InlineKeyboardMarkup,InlineKeyboardButton
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from os import environ, remove
 from threading import Thread
 from json import load
-from re import search
+from texts import gdrivetext, otherstext, ddltext, shortnertext, HELP_TEXT
 from texts import HELP_TEXT
 from ddl import ddllist, direct_link_generator
 from time import time
 
 app = Flask(__name__)
 
+
 def handle_index(ele):
     result = bypasser.scrapeIndex(ele)
 
 
-
 def store_shortened_links(link):
-    with open('shortened_links.txt', 'a') as file:
-        file.write(link + '\n')
+    with open("shortened_links.txt", "a") as file:
+        file.write(link + "\n")
 
 
 def loop_thread(url):
@@ -55,21 +53,42 @@ def loop_thread(url):
 
     return link
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         url = request.form.get("url")
         result = loop_thread(url)
 
-        resp = make_response(render_template("index.html", result=result))
-        resp.set_cookie('shortened_links', result)
+        shortened_links = request.cookies.get("shortened_links")
+        if shortened_links:
+            prev_links = shortened_links.split(",")
+        else:
+            prev_links = []
+
+        if result:
+            prev_links.append(result)
+
+            if len(prev_links) > 10:
+                prev_links = prev_links[-10:]
+
+        shortened_links_str = ",".join(prev_links)
+
+        resp = make_response(
+            render_template("index.html", result=result, prev_links=prev_links)
+        )
+        resp.set_cookie("shortened_links", shortened_links_str)
 
         return resp
 
-    shortened_links = request.cookies.get('shortened_links')
-    return render_template("index.html", result=None, shortened_links=shortened_links.split(",") if shortened_links else None)
+    shortened_links = request.cookies.get("shortened_links")
+    return render_template(
+        "index.html",
+        result=None,
+        prev_links=shortened_links.split(",") if shortened_links else None,
+    )
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host="0.0.0.0", port=port)
