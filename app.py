0bb1b65cf1f4cd2d2ef1386232c28844a5da770d
sujal <sujalpatel7574@gmail.com>
2023-08-11 18:52:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,14 +51,33 @@ def index():
     if request.method == "POST":
         url = request.form.get("url")
         result = loop_thread(url)
+        
+     
+        shortened_links = request.cookies.get('shortened_links')
+        if shortened_links:
+            prev_links = shortened_links.split(',')
+        else:
+            prev_links = []
 
-        resp = make_response(render_template("index.html", result=result))
-        resp.set_cookie('shortened_links', result)
+       
+        if result:
+            prev_links.append(result)
+           
+            if len(prev_links) > 10: 
+                prev_links = prev_links[-10:]  
+
+       
+        shortened_links_str = ','.join(prev_links)
+
+        
+        resp = make_response(render_template("index.html", result=result, prev_links=prev_links))
+        resp.set_cookie('shortened_links', shortened_links_str)
 
         return resp
 
     shortened_links = request.cookies.get('shortened_links')
-    return render_template("index.html", result=None, shortened_links=shortened_links.split(",") if shortened_links else None)
+    return render_template("index.html", result=None, prev_links=shortened_links.split(",") if shortened_links else None)
+
 
 
 if __name__ == "__main__":
