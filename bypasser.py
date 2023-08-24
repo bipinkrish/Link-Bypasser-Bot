96f7a83dd14721c4bb292b47760719a9faa8fc6a
EverTel <139523580+evertel@users.noreply.github.com>
2023-08-24 19:58:03 +0530

    Fix moneykamalo

--- bypasser.py
+++ bypasser.py
@@ -1822,7 +1822,7 @@ def moneykamalo(url):
     url = url[:-1] if url[-1] == '/' else url
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
-    ref = "https://techkeshri.com/"
+    ref = "https://blog.techkeshri.com/"
     h = {"referer": ref}
     resp = client.get(final_url,headers=h)
     soup = BeautifulSoup(resp.content, "html.parser")
