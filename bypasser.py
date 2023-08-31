d7d3b1d2ff41e6756465ccae79974e31adf9b399
Sujal Patel <76617735+SujalPatel75@users.noreply.github.com>
2023-08-31 19:25:37 +0530

    Updated xpshort

--- bypasser.py
+++ bypasser.py
@@ -1610,7 +1610,7 @@ def xpshort(url):
     url = url[:-1] if url[-1] == '/' else url
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
-    ref = "https://www.twinthrottlers.xyz/"
+    ref = "https://www.animalwallpapers.online/"
     h = {"referer": ref}
     resp = client.get(final_url,headers=h)
     soup = BeautifulSoup(resp.content, "html.parser")
