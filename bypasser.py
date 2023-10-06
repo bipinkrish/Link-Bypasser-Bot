c404a00d9ce633fa61d064b9c2bbc326863beb45
Bipin <bipinkrish4@gmail.com>
2023-10-06 10:13:03 +0530

    filecrypt fix

--- bypasser.py
+++ bypasser.py
@@ -928,21 +928,11 @@ def filecrypt(url):
     "accept-language": "en-US,en;q=0.9",
     "cache-control": "max-age=0",
     "content-type": "application/x-www-form-urlencoded",
-    "dnt": "1",
     "origin": "https://filecrypt.co",
     "referer": url,
-    "sec-ch-ua": '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "Windows",
-    "sec-fetch-dest": "document",
-    "sec-fetch-mode": "navigate",
-    "sec-fetch-site": "same-origin",
-    "sec-fetch-user": "?1",
-    "upgrade-insecure-requests": "1",
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36" 
     }
     
-
     resp = client.get(url, headers=headers)
     soup = BeautifulSoup(resp.content, "html.parser")
 
@@ -954,7 +944,7 @@ def filecrypt(url):
             break
 
     resp = client.get(dlclink,headers=headers)
-    return getlinks(resp.text,client)
+    return getlinks(resp.text)
 
 
 #####################################################
