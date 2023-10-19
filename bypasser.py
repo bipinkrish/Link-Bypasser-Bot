ba974650b550a16efd2227388e4f26910c77c4f8
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-10-19 07:17:39 +0530

    Update bypasser.py (#156)
    onepagelink Fixed

--- bypasser.py
+++ bypasser.py
@@ -1659,14 +1659,14 @@ def onepagelink(url):
     url = url[:-1] if url[-1] == "/" else url
     code = url.split("/")[-1]
     final_url = f"https://{DOMAIN}/{code}"
-    ref = "gorating.in"
+    ref = "market.gorating.in"
     h = {"referer": ref}
     response = client.get(final_url, headers=h)
     soup = BeautifulSoup(response.text, "html.parser")
     inputs = soup.find_all("input")
     data = {input.get("name"): input.get("value") for input in inputs}
     h = {"x-requested-with": "XMLHttpRequest"}
-    time.sleep(9)
+    time.sleep(5)
     r = client.post(f"https://{DOMAIN}/links/go", data=data, headers=h)
     try:
         return r.json()["url"]
