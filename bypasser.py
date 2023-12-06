d960abcc0c017bfcd231eeaef18e5d3d641865b8
Ashu782 <153031287+Ashu782@users.noreply.github.com>
2023-12-06 07:49:38 +0530

    MoneyKamalo Fixed (#159)

--- bypasser.py
+++ bypasser.py
@@ -1812,7 +1812,7 @@ def moneykamalo(url):
     url = url[:-1] if url[-1] == '/' else url
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
-    ref = "https://blog.techkeshri.com/"
+    ref = "https://bloging.techkeshri.com/"
     h = {"referer": ref}
     resp = client.get(final_url,headers=h)
     soup = BeautifulSoup(resp.content, "html.parser")
