26c17bb257a9ac8308b65a0f7febc94b1da43369
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-12-09 01:07:50 +0530

    Update bypasser.py (#160)
    New Site Added: mdisky.link

--- bypasser.py
+++ bypasser.py
@@ -2062,6 +2062,27 @@ def vipurl(url):
         return r.json()["url"]
     except BaseException:
         return "Something went wrong :("
+##################################################################################################### 
+#mdisky.link
+def mdisky(url):
+    client = cloudscraper.create_scraper(allow_brotli=False)
+    DOMAIN = "https://go.bloggingaro.com/"
+    url = url[:-1] if url[-1] == "/" else url
+    code = url.split("/")[-1]
+    final_url = f"{DOMAIN}/{code}"
+    ref = "https://marioworlds.bloggingaro.com/"
+    h = {"referer": ref}
+    resp = client.get(final_url, headers=h)
+    soup = BeautifulSoup(resp.content, "html.parser")
+    inputs = soup.find_all("input")
+    data = {input.get("name"): input.get("value") for input in inputs}
+    h = {"x-requested-with": "XMLHttpRequest"}
+    time.sleep(6)
+    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
+    try:
+        return str(r.json()["url"])
+    except BaseException:
+        return "Something went wrong :("
 
 ##################################################################################################### 
 # bitly + tinyurl
@@ -2359,6 +2380,11 @@ def shortners(url):
     elif "link.vipurl.in" in url or "count.vipurl.in" in url or "vipurl.in" in url:
         print("entered vipurl:",url)
         return vipurl(url)
+
+    # mdisky
+    elif "mdisky.link" in url:
+        print("entered mdisky:", url)
+        return mdisky(url)
         
     # htpmovies sharespark cinevood
     elif "https://htpmovies." in url or 'https://sharespark.me/' in url or "https://cinevood." in url or "https://atishmkv." in url \
