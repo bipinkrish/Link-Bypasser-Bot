f17f0b483ac9fbafdc9dd93f07d70fbbae792c27
Codaangi <121353390+Jegaa1@users.noreply.github.com>
2024-05-04 12:32:16 +0530

    Update bypasser.py
    Tnshort Fixed

--- bypasser.py
+++ bypasser.py
@@ -1975,14 +1975,14 @@ def tnshort(url):
     url = url[:-1] if url[-1] == "/" else url
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
-    ref = "https://jrlinks.in/"
+    ref = "https://movies.djnonstopmusic.in/"
     h = {"referer": ref}
     resp = client.get(final_url, headers=h)
     soup = BeautifulSoup(resp.content, "html.parser")
     inputs = soup.find_all("input")
     data = {input.get("name"): input.get("value") for input in inputs}
     h = {"x-requested-with": "XMLHttpRequest"}
-    time.sleep(9)
+    time.sleep(8)
     r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
     try:
         return str(r.json()["url"])
