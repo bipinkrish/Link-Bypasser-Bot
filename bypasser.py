6c006d56b77b573a37d2cd21ffb52a83bcc50783
Codaangi <121353390+Jegaa1@users.noreply.github.com>
2024-09-30 20:33:49 +0530

    Update bypasser.py
    Tmvalue Fixed again

--- bypasser.py
+++ bypasser.py
@@ -2272,7 +2272,7 @@ def tnvalue(url):
     url = url[:-1] if url[-1] == "/" else url
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
-    ref = "https://dailynetflixcookies.in/"
+    ref = "https://ladkibahin.com/"
     h = {"referer": ref}
     resp = client.get(final_url, headers=h)
     soup = BeautifulSoup(resp.content, "html.parser")
